6c97f8584ee8057aba8124142c0587f353b7850d
Maganga Mwambonu <codebymaganga@gmail.com>
2024-03-23 12:57:34 +0300

    Revert "node modules"
    This reverts commit 8a795d796677d1c361a5568818404062eca05bdb.

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development" ? "/api" : process.env.BASE_URL;
 
-// Update the production URL
+// // Update the production URL
 // if (process.env.NODE_ENV === "production") {
 //   BASE_URL = "https://hr-system-backend-1.onrender.com/";
 // }
